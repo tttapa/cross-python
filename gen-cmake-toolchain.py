a1ae5db56bbafadf4615b016bf8aa40232117732
Pieter Pas <pieter.pas@outlook.com>
2023-01-21 19:24:08 +0100

    Add Clang support too CMake toolchain file

--- gen-cmake-toolchain.py
+++ gen-cmake-toolchain.py
@@ -21,15 +21,6 @@ set(CROSS_GNU_TRIPLE "{CROSS_GNU_TRIPLE}"
     CACHE STRING "The GNU triple of the toolchain to use")
 set(CMAKE_LIBRARY_ARCHITECTURE {CMAKE_LIBRARY_ARCHITECTURE})
 
-# Toolchain
-set(TOOLCHAIN_DIR "${{CMAKE_CURRENT_LIST_DIR}}/../x-tools/${{CROSS_GNU_TRIPLE}}")
-set(CMAKE_C_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-gcc"
-    CACHE FILEPATH "C compiler")
-set(CMAKE_CXX_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-g++"
-    CACHE FILEPATH "C++ compiler")
-set(CMAKE_Fortran_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-gfortran"
-    CACHE FILEPATH "Fortran compiler")
-
 # Compiler flags
 set(CMAKE_C_FLAGS_INIT       "{ARCH_FLAGS}")
 set(CMAKE_CXX_FLAGS_INIT     "{ARCH_FLAGS}")
@@ -44,6 +35,92 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 # Packaging
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "{CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
 
+# Toolchain and sysroot
+set(TOOLCHAIN_DIR "${{CMAKE_CURRENT_LIST_DIR}}/../x-tools/${{CROSS_GNU_TRIPLE}}")
+set(CMAKE_SYSROOT "${{TOOLCHAIN_DIR}}/${{CROSS_GNU_TRIPLE}}/sysroot")
+
+# Clang toolchain
+option(TOOLCHAIN_USE_CLANG "Use Clang instead of GCC" Off)
+if (TOOLCHAIN_USE_CLANG)
+    # Select the GCC toolchain to use
+    set(TOOLCHAIN_C_COMPILER ${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-gcc)
+
+    # Find Clang
+    set(TOOLCHAIN_CLANG_PREFIX "" CACHE STRING "Prefix to the Clang command")
+    set(TOOLCHAIN_CLANG_SUFFIX "" CACHE STRING "Suffix to the Clang command")
+    set(TOOLCHAIN_C_COMPILER_CLANG ${{TOOLCHAIN_CLANG_PREFIX}}clang${{TOOLCHAIN_CLANG_SUFFIX}}
+        CACHE FILEPATH "Full name or path of the clang command")
+    set(TOOLCHAIN_CXX_COMPILER_CLANG ${{TOOLCHAIN_CLANG_PREFIX}}clang++${{TOOLCHAIN_CLANG_SUFFIX}}
+        CACHE FILEPATH "Full name or path of the clang++ command")
+    set(TOOLCHAIN_Fortran_COMPILER_CLANG ${{TOOLCHAIN_CLANG_PREFIX}}flang-new${{TOOLCHAIN_CLANG_SUFFIX}}
+        CACHE FILEPATH "Full name or path of the flang command")
+    # Use Clang as the cross-compiler
+    set(CMAKE_C_COMPILER ${{TOOLCHAIN_C_COMPILER_CLANG}}
+        CACHE FILEPATH "C compiler")
+    set(CMAKE_CXX_COMPILER ${{TOOLCHAIN_CXX_COMPILER_CLANG}}
+        CACHE FILEPATH "C++ compiler")
+    set(CMAKE_Fortran_COMPILER ${{TOOLCHAIN_Fortran_COMPILER_CLANG}}
+        CACHE FILEPATH "Fortran compiler")
+
+    # Get the machine triple from GCC
+    execute_process(COMMAND ${{TOOLCHAIN_C_COMPILER}} -dumpmachine
+                    OUTPUT_VARIABLE CROSS_GNU_TRIPLE_EFFECTIVE
+                    ERROR_VARIABLE CROSS_GNU_TRIPLE_EFFECTIVE_ERROR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(CROSS_GNU_TRIPLE_EFFECTIVE ${{CROSS_GNU_TRIPLE_EFFECTIVE}}
+        CACHE STRING "The GNU triple of the toolchain actually in use")
+    if (NOT CROSS_GNU_TRIPLE_EFFECTIVE)
+        message(FATAL_ERROR "Unable to determine GCC triple ${{CROSS_GNU_TRIPLE_EFFECTIVE}} ${{CROSS_GNU_TRIPLE_EFFECTIVE_ERROR}}")
+    endif()
+
+    # Get the installation folder from GCC
+    execute_process(COMMAND ${{TOOLCHAIN_C_COMPILER}} -print-search-dirs
+                    OUTPUT_VARIABLE TOOLCHAIN_GCC_INSTALL
+                    ERROR_VARIABLE TOOLCHAIN_GCC_INSTALL_ERROR)
+    string(REGEX MATCH "(^|\\r|\\n)install: +([^\\r\\n]*)" 
+        TOOLCHAIN_GCC_INSTALL_LINE ${{TOOLCHAIN_GCC_INSTALL}})
+    if (NOT TOOLCHAIN_GCC_INSTALL_LINE)
+        message(FATAL_ERROR "Unable to determine GCC installation ${{TOOLCHAIN_GCC_INSTALL}} ${{TOOLCHAIN_GCC_INSTALL_ERROR}}")
+    endif()
+    cmake_path(SET TOOLCHAIN_GCC_INSTALL_LIB NORMALIZE ${{CMAKE_MATCH_2}})
+    cmake_path(SET TOOLCHAIN_GCC_INSTALL NORMALIZE ${{CMAKE_MATCH_2}})
+    cmake_path(APPEND TOOLCHAIN_GCC_INSTALL "../../../..")
+    cmake_path(ABSOLUTE_PATH TOOLCHAIN_GCC_INSTALL)
+    set(TOOLCHAIN_GCC_INSTALL ${{TOOLCHAIN_GCC_INSTALL}}
+        CACHE PATH "Path to GCC installation")
+    message(STATUS "Using Clang toolchain with GCC installation ${{TOOLCHAIN_GCC_INSTALL}}")
+
+    # Find a linker
+    find_program(TOOLCHAIN_LINKER ${{CROSS_GNU_TRIPLE}}-ld REQUIRED
+        HINTS ${{TOOLCHAIN_DIR}}/bin)
+
+    # Specify architecture-specific flags
+    set(ARCH_FLAGS "-target ${{CROSS_GNU_TRIPLE_EFFECTIVE}}")
+    # Make sure that Clang finds the GCC installation and a suitable linker
+    set(TOOLCHAIN_FLAGS "--gcc-toolchain=${{TOOLCHAIN_GCC_INSTALL}}")
+    set(TOOLCHAIN_LINK_FLAGS "-L${{TOOLCHAIN_GCC_INSTALL_LIB}} -fuse-ld=${{TOOLCHAIN_LINKER}}")
+    # Runtime libraries for Flang
+    set(FLANG_LIB_DIR "${{CMAKE_CURRENT_LIST_DIR}}/../flang-main/usr/local/lib")
+    string(APPEND TOOLCHAIN_LINK_FLAGS " -L${{FLANG_LIB_DIR}}")
+    # Compilation flags
+    string(APPEND CMAKE_C_FLAGS_INIT " ${{ARCH_FLAGS}} ${{TOOLCHAIN_FLAGS}}")
+    string(APPEND CMAKE_CXX_FLAGS_INIT " ${{ARCH_FLAGS}} ${{TOOLCHAIN_FLAGS}}")
+    string(APPEND CMAKE_Fortran_FLAGS_INIT " ${{ARCH_FLAGS}} ${{TOOLCHAIN_FLAGS}}")
+    string(APPEND CMAKE_Fortran_FLAGS_INIT " --sysroot=${{CMAKE_SYSROOT}}")
+    # Linker flags
+    string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT " ${{TOOLCHAIN_LINK_FLAGS}}")
+    string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT " ${{TOOLCHAIN_LINK_FLAGS}}")
+    string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT " ${{TOOLCHAIN_LINK_FLAGS}}")
+# GCC toolchain
+else()
+    set(CMAKE_C_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-gcc"
+        CACHE FILEPATH "C compiler")
+    set(CMAKE_CXX_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-g++"
+        CACHE FILEPATH "C++ compiler")
+    set(CMAKE_Fortran_COMPILER "${{TOOLCHAIN_DIR}}/bin/${{CROSS_GNU_TRIPLE}}-gfortran"
+        CACHE FILEPATH "Fortran compiler")
+endif()
+
 # Locating Python
 find_package(Python3 REQUIRED COMPONENTS Interpreter)
 set(Python3_VERSION_MAJ_MIN "${{Python3_VERSION_MAJOR}}.${{Python3_VERSION_MINOR}}")
@@ -55,7 +132,7 @@ if (Python3_INTERPRETER_ID MATCHES "PyPy")
     set(PYTHON_STAGING_DIR "${{CMAKE_CURRENT_LIST_DIR}}/../pypy${{Python3_VERSION_MAJ_MIN}}")
     set(Python3_LIBRARY "${{PYTHON_STAGING_DIR}}/bin/libpypy${{Python3_PyPy_LIB_VERSION}}-c.so")
     set(Python3_INCLUDE_DIR "${{PYTHON_STAGING_DIR}}/include/pypy${{Python3_VERSION_MAJ_MIN}}")
-    string(REGEX MATCH "([0-9]+)\.([0-9]+).*" _ ${{Python3_PyPy_VERSION}})
+    string(REGEX MATCH "([0-9]+)\\.([0-9]+).*" _ ${{Python3_PyPy_VERSION}})
     set(PY_BUILD_EXT_SUFFIX ".pypy${{Python3_VERSION_MAJOR}}${{Python3_VERSION_MINOR}}-pp${{CMAKE_MATCH_1}}${{CMAKE_MATCH_2}}-${{CMAKE_SYSTEM_PROCESSOR}}-linux-gnu.so")
 else()
     execute_process(COMMAND ${{Python3_EXECUTABLE}}
